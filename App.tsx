8f10a302582c710ac244a95a29eac8d2d19b31d8
Taysmar <taysmar@outlook.com>
2021-04-19 20:23:49 -0300

    Finalização da aula 1, NLW, modificações na pagina welcome

--- App.tsx
+++ App.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Welcome } from './src/pages/Welcome';
+
 
 export default function App() {
   return(
-    <View style={style.container}>
-      <Text>NLW 5</Text>
-    </View>
+    <Welcome />
+
   )
 }
 
-const style = StyleSheet.create({
- container: {
-   flex:1, 
-   justifyContent: 'center', 
-   alignItems: 'center'
-  }
-})
