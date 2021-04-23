e56afeec7719b09b03d47e0e2777823622c56ddb
Taysmar <taysmar@outlook.com>
2021-04-23 19:28:16 -0300

    Aula 3 da NLW finalizada, opção de filtro adicionada e melhora na experiencia do usuário

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/core';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 import { Button } from '../components/Button';
@@ -6,6 +7,11 @@ import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Confirmation() {
+    const navigation = useNavigation();
+  
+    function handleMoveOn() {
+        navigation.navigate('PlantSelect');
+    }
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.content} >
@@ -23,6 +29,7 @@ export function Confirmation() {
                 <View style={styles.footer}>
                     <Button 
                     title="Começar"
+                    onPress={handleMoveOn}
                     
                     />
                 </View>
